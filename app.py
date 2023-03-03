fc58e280612ef0b733a293c085645d5eb666b5d5
ivan_stand_l <ivansyn2013@yandex.ru>
2023-03-03 23:13:44 +0300

    add color to messages add logging

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from views.table import table_app
 from config.config import DevelopmentConfig
 from commands.my_commands import my_commands_app
+from views.auth import auth_app
 
 #Create app
 app = Flask(__name__)
@@ -9,14 +10,15 @@ app = Flask(__name__)
 #Blueprints
 app.register_blueprint(table_app, url_prefix='/')
 app.register_blueprint(my_commands_app)
+app.register_blueprint(auth_app, url_prefix="/auth")
 
 #Config
 app.config.from_object(DevelopmentConfig)
 
 
-@app.route('/hi_world')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/')
+def index():  # put application's code here
+    return render_template('main/index.html')
 
 
 

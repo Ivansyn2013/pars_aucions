1ba33787f9c2f027ff2fcdea09dae9c9b56f1b8a
ivan_stand_l <ivansyn2013@yandex.ru>
2023-03-05 20:25:05 +0300

    auth by login done!

--- views/table.py
+++ views/table.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
 from logic.common_main import get_data
+from flask_login import login_required
 table_app = Blueprint('table_app', __name__)
 
 
 @table_app.route('/table/', endpoint='/table')
+@login_required
 def table_render():
     data = get_data()
     return render_template('main/table.html',

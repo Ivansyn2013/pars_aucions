a8dca141c14a6f4dc8a3af0affdd907a8e5b8d24
ivan_stand_l <ivansyn2013@yandex.ru>
2023-03-05 20:54:40 +0300

    start work on table

--- views/table.py
+++ views/table.py
@@ -1,12 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from logic.common_main import get_data
 from flask_login import login_required
 table_app = Blueprint('table_app', __name__)
 
 
-@table_app.route('/table/', endpoint='/table')
+@table_app.route('/table/', methods=["GET","POST"], endpoint='/table')
 @login_required
 def table_render():
-    data = get_data()
-    return render_template('main/table.html',
-                           data=data)
+
+    if request.method == "GET":
+        data = get_data()
+        return render_template('main/table.html',
+                               data=data)
+    elif request.method == "POST":
+        data = get_data(request.form.get('claim_number'))
+        return render_template('main/table.html',
+                               data=data)
